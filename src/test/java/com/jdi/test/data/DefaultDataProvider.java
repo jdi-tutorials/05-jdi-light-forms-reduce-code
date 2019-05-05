9a9c297b1ff592b1390ac70f8d2c2167542091d8
RomanIovlev <Roman_Iovlev@epam.com>
2019-05-05 21:16:19 +0300

    correct data

--- src/test/java/com/jdi/test/data/DefaultDataProvider.java
+++ src/test/java/com/jdi/test/data/DefaultDataProvider.java
@@ -10,25 +10,15 @@ public class DefaultDataProvider {
     );
 
     public static ContactInfo SIMPLE_CONTACT = new ContactInfo().set(c -> {
-        c.name = "Roman";
-        c.lastName = "Iovlev";
-        c.position = "ChiefQA";
-        c.passportNumber = 4321;
-        c.passportSeria = 123456;
+        c.name = "Roman"; c.lastName = "Iovlev"; c.position = "ChiefQA";
+        c.passportNumber = 4321; c.passportSeria = 123456;
         c.description = "JDI - awesome UI autoamtion tool"; }
     );
 
     public static ContactInfo FULL_CONTACT = new ContactInfo().set(c -> {
-        c.name = "Roman";
-        c.lastName = "Full Contact";
-        c.position = "ChiefQA";
-        c.passportNumber = 4321;
-        c.passportSeria = 123456;
-        c.description = "JDI - awesome UI automation tool";
-
-        c.gender = "Female";
-        c.religion = "Other";
-        c.weather = "Sun, Snow";
-        c.acceptConditions = true; }
+        c.name = "Roman"; c.lastName = "Full Contact"; c.position = "ChiefQA";
+        c.religion = "Other"; c.weather = "Sun, Snow"; c.acceptConditions = true;
+        c.gender = "Female"; c.passportNumber = 4321; c.passportSeria = 123456;
+        c.description = "JDI - awesome UI automation tool"; }
     );
 }
