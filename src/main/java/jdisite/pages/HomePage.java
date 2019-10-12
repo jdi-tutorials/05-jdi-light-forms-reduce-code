4900e25348f0e4988b9cfe343db21bfaf4f3253d
Roman_Iovlev <roman_iovlev@epam.com>
2019-10-12 08:09:22 +0300

    remove homepage

--- src/main/java/jdisite/pages/HomePage.java
+++
@@ -1,8 +0,0 @@
-package jdisite.pages;
-
-import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.Url;
-
-@Url("/")
-public class HomePage extends WebPage {
-}
